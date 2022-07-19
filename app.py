0a3f6eadd0276e69fbd6b2b491952636bb78c103
Rayan Khan <rayan.ahkhan@gmail.com>
2022-07-19 23:09:23 +0300

    sets up file structure and stuff

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import os
+import requests
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
